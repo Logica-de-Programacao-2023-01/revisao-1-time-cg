4343c401fb53ea76e83c2cac1a922d6ed441e148
iza <126572391+izabellealencar05@users.noreply.github.com>
2023-06-09 10:46:31 -0300

    Update q2.go

--- q2/q2.go
+++ q2/q2.go
@@ -1,6 +1,18 @@
 package q2
 
 func AverageLettersPerWord(text string) (float64, error) {
-	// Implemente sua solução aqui
-	return 0, nil
+	simbolos := []string{",", ".", "?", "!", ";", ":"}
+	list := strings.Fields(text)
+	soma := 0
+	if text == "" {
+		return 0, errors.New("textp vazio")
+	}
+	for i := 0; i < len(simbolos); i++ {
+		text = strings.ReplaceAll(text, simbolos[i], "")
+	}
+
+	for i := 0; i < len(list); i++ {
+		soma += len(list[i])
+	}
+	return float64(soma) / float64(len(list)), nil
 }
