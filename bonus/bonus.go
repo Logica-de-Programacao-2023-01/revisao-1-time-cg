9975a5592358147112183b74be30e5fe5b7df010
iza <126572391+izabellealencar05@users.noreply.github.com>
2023-06-10 11:40:48 -0300

    Update bonus.go

--- bonus/bonus.go
+++ bonus/bonus.go
@@ -1,6 +1,33 @@
 package bonus
+import (
+	"fmt"
+)
+const (
+	dano          = 7
+	maxdifference = 20
+)
 
 func CalculateDamage(characterLevel, enemyLevel int) (int, error) {
-	// Implemente sua solução aqui
-	return 0, nil
+	if characterLevel <= 0 || enemyLevel <= 0 {
+		return 0, fmt.Errorf("nivel invalido")
+	}
+	dano := characterLevel * dano
+
+	diferençanivel := characterLevel - enemyLevel
+	if diferençanivel > maxdifference {
+		dano *= 5
+	} else if diferençanivel < -maxdifference {
+		dano *= 2
+	}
+	if characterLevel > enemyLevel {
+		dano *= 10
+	} else if characterLevel < enemyLevel {
+		dano *= 5
+	}
+	if characterLevel > 100 {
+		dano *= 20
+	} else if enemyLevel > 100 {
+		dano *= 2
+	}
+	return dano, nil
 }
